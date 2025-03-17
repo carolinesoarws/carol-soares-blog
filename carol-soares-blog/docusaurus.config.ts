4ebca1f9ddd8ff1696194650ea9811d10d8dd810
cssfqbx <karol.siva@hotmail.com>
2025-03-17 20:51:56 -0300

    ajuste build

--- carol-soares-blog/docusaurus.config.ts
+++ carol-soares-blog/docusaurus.config.ts
@@ -9,10 +9,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://carolinesoarws.github.io/',
+  url: 'https://carolinesoarws.github.io/carol-soares-blog/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/carol-soares-blog/',
+  baseUrl: '/docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
