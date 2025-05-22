49854780ebb2787294b903019662dbe7f4ceadf7
carolinesoarws <karol.siva@hotmail.com>
2025-05-21 21:27:31 -0300

    Update documentation

--- assets/js/c08cfdba.2b759d82.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkcarol_soares_blog=self.webpackChunkcarol_soares_blog||[]).push([[7248],{9506:o=>{o.exports=JSON.parse('{"metadata":{"permalink":"/carol-soares-blog/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
